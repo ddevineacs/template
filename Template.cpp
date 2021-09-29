be94851f9a441cea210ba24fc6e8b8741ac3f47e
ddevineacs <90146392+ddevineacs@users.noreply.github.com>
2021-09-29 08:49:40 -0400

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,22 @@
+/* David Devine ch
+
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    const int
+    const double
+    int
+    double
+    cout <<
+    cin >>
+    << endl;
+    fixed << setprecision(2)
+    right/left << setw()
+
+    return 0;
+}
